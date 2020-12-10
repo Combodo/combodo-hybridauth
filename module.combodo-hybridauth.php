ed6e36ad3091901c1323c276a391cb9b7ab4a83f
Eric <eric.espie@combodo.com>
2020-12-10 11:58:59 +0100

    1.0.2

--- module.combodo-hybridauth.php
+++ module.combodo-hybridauth.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-hybridauth/1.0.1',
+	'combodo-hybridauth/1.0.2',
 	array(
 		// Identification
 		//
