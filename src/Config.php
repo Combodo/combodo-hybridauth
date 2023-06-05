2e04e0ac156894c251aad305430a8addab064d87
Timothee <timothee.callet@combodo.com>
2023-06-05 17:36:49 +0200

    N°6369 Fixing warnings when configuration is missing

--- src/Config.php
+++ src/Config.php
@@ -19,7 +19,7 @@ class Config
 
 	public static function Get($sName)
 	{
-		$sValue = MetaModel::GetModuleSetting('combodo-hybridauth', $sName, '');
+		$sValue = MetaModel::GetModuleSetting('combodo-hybridauth', $sName, []);
 		return $sValue;
 	}
 
