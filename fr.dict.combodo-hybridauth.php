5c2fb27481050faf3c203eac5c61018f6799bdcf
Timothee <timothee.callet@combodo.com>
2023-06-14 14:45:51 +0200

    Dictionaries update for 3.1 beta

--- fr.dict.combodo-hybridauth.php
+++ fr.dict.combodo-hybridauth.php
@@ -5,9 +5,8 @@
  * @copyright   Copyright (C) 2013 XXXXX
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
-
 Dict::Add('FR FR', 'French', 'Français', array(
 	'HybridAuth:Error:UserNotAllowed' => 'Utilisateur non autorisé',
-    'HybridAuth:Login:SignIn' => 'S\'identifier avec %1$s',
-    'HybridAuth:Login:SignInTooltip' => 'Cliquer ici pour s\'identifier avec %1$s',
+	'HybridAuth:Login:SignIn' => 'S\'identifier avec %1$s',
+	'HybridAuth:Login:SignInTooltip' => 'Cliquer ici pour s\'identifier avec %1$s',
 ));
