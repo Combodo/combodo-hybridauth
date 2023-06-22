e26e10a6dc2d2dd800890168254f13777cb5fa76
Lars Kaltefleiter <lars.kaltefleiter@itomig.de>
2023-06-22 20:46:43 +0200

    :globe_with_meridians: Updated german translations

--- de.dict.combodo-hybridauth.php
+++ de.dict.combodo-hybridauth.php
@@ -6,7 +6,7 @@
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
 Dict::Add('DE DE', 'German', 'Deutsch', array(
-	'HybridAuth:Error:UserNotAllowed' => 'User not allowed~~',
-	'HybridAuth:Login:SignIn' => 'Sign in with %1$s~~',
-	'HybridAuth:Login:SignInTooltip' => 'Click here to authenticate yourself with %1$s~~',
+	'HybridAuth:Error:UserNotAllowed' => 'Benutzer nicht zugelassen',
+	'HybridAuth:Login:SignIn' => 'Anmelden mit %1$s',
+	'HybridAuth:Login:SignInTooltip' => 'Klicken Sie hier, um sich zu authentifizieren mit %1$s',
 ));
