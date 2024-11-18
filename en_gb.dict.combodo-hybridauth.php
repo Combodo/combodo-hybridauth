3fc2a0bce88f3e20a249b25c1c9272af9381b663
Lars Kaltefleiter <lars.kaltefleiter@itomig.de>
2024-11-18 10:57:30 +0100

    Add EN GB dict file (#5)

---
+++ en_gb.dict.combodo-hybridauth.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN GB', 'British English', 'British English', array(
+	'HybridAuth:Error:UserNotAllowed' => 'User not allowed',
+	'HybridAuth:Login:SignIn' => 'Sign in with %1$s',
+	'HybridAuth:Login:SignInTooltip' => 'Click here to authenticate yourself with %1$s',
+));
