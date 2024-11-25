ea02f040ae36a1b6e4fd30438c9704bc866d76e2
odain <olivier.dain@combodo.com>
2024-11-25 16:30:58 +0100

    N°7673 - Oauth2 for webhook - lib hybrodauth moved in combodo-oauth2-client

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,6 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Hybridauth\\' => array($vendorDir . '/hybridauth/hybridauth/src'),
     'Combodo\\iTop\\HybridAuth\\' => array($baseDir . '/src'),
 );
