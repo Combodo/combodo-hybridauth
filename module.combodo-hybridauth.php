dd3540b84845d94811a19c370bd8d10aa498c0e6
odain <olivier.dain@combodo.com>
2025-03-03 15:22:05 +0100

    set 1.2.3-1

--- module.combodo-hybridauth.php
+++ module.combodo-hybridauth.php
@@ -10,7 +10,7 @@
 //
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-hybridauth/1.2.3',
+	'combodo-hybridauth/1.2.3-1',
 	array(
 		// Identification
 		//
