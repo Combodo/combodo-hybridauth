9b5ed9c7ea4b43a4aebee9407039007c125b75f5
odain <olivier.dain@combodo.com>
2026-05-15 16:05:31 +0200

    N°9576 - propose disabled Keycloak default conf

--- module.combodo-hybridauth.php
+++ module.combodo-hybridauth.php
@@ -59,28 +59,52 @@ SetupWebPage::AddModule(
             'debug' => false,
             'synchronize_user' => false,
             'synchronize_contact' => false,
-            'default_organization' => '',
-            'default_profiles' => ['Portal User', 'Configuration Manager'],
+            'default_organization' => '', //you must fill it here or at provider level for contact provisioning
+            'default_profiles' => ['Portal User', 'Configuration Manager'], //must be set here or at provider level for user provisioning
+			'default_allowed_orgs' => [], //leave it empty here AND at provider level to disable allowed orgs provisioning
 			'providers' => array(
+			  'Keycloak' =>
+              array (
+                'keys' =>
+                array (
+                  'id' => 'my-clientid',
+                  'secret' => 'my-secret',
+                ),
+                'enabled' => false,
+                'debug' => true,
+                'url' => 'keycloak_url',
+                'realm' => 'my_realm',
+                'label' => 'keycloak',
+                'tooltip' => 'click to authenticate through keycloak',
+                'icon_url' => '',
+                'synchronize_contact' => false,
+                'refresh_existing_contact' => false,
+				'default_organization' => null,
+                'synchronize_user' => false,
+                'refresh_existing_user' => false,
+				'default_profiles' => null,
+                'logout_before_disconnect' => false, //to logout from Keycloak when quiting iTop
+
+				//for advanced provisioning
+                'provide_all_getuserprofile_output' => false, //to pass all IdP GetUserProfile fields
+
+				'org_idp_key' => 'organization', //field fetched in IdP GetUserProfile answer
+				'org_oql_search_field' => 'name', //iTop field to reconciliate org (defaut name)
+
+				'profiles_idp_key' => null, //advanced profile provisioning
+				'profiles_idp_separator' => null, //advanced profile provisioning
+				'groups_to_profiles' => null, //advanced profile provisioning
+
+				'allowed_orgs_idp_key' => null, //advanced allowed orgs provisioning
+				'allowed_orgs_idp_separator' => null, //advanced allowed orgs provisioning
+				'allowed_orgs_oql_search_field' => null, //advanced allowed orgs provisioning
+				'groups_to_orgs' => null, //advanced allowed orgs provisioning
+              ),
 				'Google' => array(
 					'enabled' => false,
 					'keys' => array(
 						'id'     => 'your-google-client-id',
 						'secret' => 'your-google-client-secret'
-					),
-				),
-				'Twitter' => array(
-					'enabled' => false,     //Optional: indicates whether to enable or disable Twitter adapter. Defaults to false
-					'keys' => array(
-						'key'    => '...', //Required: your Twitter consumer key
-						'secret' => '...'  //Required: your Twitter consumer secret
-					),
-				),
-				'Facebook' => array(
-					'enabled' => false,
-					'keys' => array(
-						'id'  => '...',
-						'secret' => '...',
 					),
 				),
 			),
